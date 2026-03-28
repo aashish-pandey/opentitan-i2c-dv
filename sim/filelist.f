1e56019ec486bcac96046d5fe5f6d5e256c3fa94
Aashish Pandey <pandeyaashish100@gmail.com>
2026-03-28 06:47:11 -0400

    closes #2 - makefile setup completed

---
+++ sim/filelist.f
@@ -0,0 +1,15 @@
+
+//RTL
+../rtl/i2c_pkg.sv
+../rtl/i2c_reg_pkg.sv 
+../rtl/reg_top.sv
+../rtl/i2c_fifos.sv 
+../rtl/i2c_fifo_sync_sram_adapter.sv 
+../rtl/i2c/bus_monitor.sv 
+../rtl/i2c_controller_fsm.sv
+../rtl/i2c_target_fsm.sv
+../rtl/i2c_core.sv
+../rtl/i2c.sv
+
+//Testbench
+../tb/top/tb_top.sv
